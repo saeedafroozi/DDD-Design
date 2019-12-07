be41783c01c763d660219b8df506a05a568d046f
unknown <afroozi@chargoon.com>
2019-12-07 09:29:29 +0330

    init repo

---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,128 @@
+﻿using Dapper.FastCrud;
+using Domain.RepositoryContract;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    internal class Repository<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey> where TEntity : class
+    {
+        private readonly IConfiguration _config;
+        private TimeSpan commandTimeout = TimeSpan.FromSeconds(120);
+        private readonly Func<SqlConnection> _dbConnectionFactory;
+
+
+        public Repository(Func<SqlConnection> dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory ?? throw new ArgumentNullException(nameof(dbConnectionFactory));
+        }
+
+        //public IDbConnection Connection
+        //{
+        //    get
+        //    {
+        //        return new SqlConnection(_config.GetConnectionString("MyConnectionString"));
+        //    }
+        //}
+
+        public async Task Insert(TEntity entity)
+        {
+            //using (IDbConnection conn = Connection)
+            //{
+            //   // string sQuery = "SELECT ID, FirstName, LastName, DateOfBirth FROM Employee WHERE ID = @ID";
+            //    conn.Open();
+            //    var result = await conn.QueryAsync<Employee>(sQuery, new { ID = id });
+            //    return result.FirstOrDefault();
+            //}
+            //Connection.Insert(entity, statement => statement.AttachToTransaction(Transaction).WithTimeout(commandTimeout));
+            using (var connection = _dbConnectionFactory.Invoke())
+            {
+                await connection.OpenAsync();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        //var result = await command(connection, transaction, Constants.CommandTimeout);
+                        await connection.InsertAsync(entity, statement => statement.AttachToTransaction(transaction).WithTimeout(commandTimeout));
+                        transaction.Commit();
+
+                        /// return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        //Logger.Instance.Error(ex);
+                        throw;
+                    }
+                }
+            }
+        }
+
+
+        //      void InsertRange(IEnumerable<TEntity> entities);
+
+
+
+
+        public async Task<bool> Update(TEntity entity)
+        {
+            using (var connection = _dbConnectionFactory.Invoke())
+            {
+                await connection.OpenAsync();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        var result = await connection.UpdateAsync(entity, statement => statement.AttachToTransaction(transaction).WithTimeout(commandTimeout));
+
+                        transaction.Commit();
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        //Logger.Instance.Error(ex);
+                        throw;
+                    }
+                }
+            }
+        }
+
+
+        //      void UpdateRange(IEnumerable<TEntity> entities);
+
+        //int Count();
+
+
+        //      bool Exists();
+
+
+        TEntity Get(TPrimaryKey primaryKey)
+        {
+            using (var connection = _dbConnectionFactory.Invoke())
+            {
+                connection.OpenAsync();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var result = connection.Find(statement=> statement.Top(1));
+                    transaction.Commit();
+                    return result;
+
+                }
+            }
+        }
+
+
+        //      TEntity Get(TPrimaryKey primaryKey, bool excludeDeleted);
+
+
+        //      TEntity Get(TEntity entity);
+    }
+}
---
+++ Domain/RepositoryContract/IRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.RepositoryContract
+{
+
+    public interface IRepository<TEntity, TPrimaryKey> where TEntity : class
+    {
+        /// <summary>
+        /// Inserts a row into the database, updating its properties based on the database generated fields.
+        /// </summary>
+        /// <param name= "entity">The entity you wish to insert.</param>
+         Task Insert(TEntity entity);
+
+        //      /// <summary>
+        //      /// Inserts a range of records in the database.
+        //      /// </summary>
+        //      /// <param name="entities">The entities you wish to insert.</param>
+        //      void InsertRange(IEnumerable<TEntity> entities);
+
+
+
+        /// <summary>
+        /// Updates a record in the database.
+        /// </summary>
+        /// <param name="entity">The entity you wish to update.</param>
+        Task<bool> Update(TEntity entity);
+
+        //      /// <summary>
+        //      /// Updates a range of records in the database.
+        //      /// </summary>
+        //      /// <param name="entities">The entities you wish to update.</param>
+        //      void UpdateRange(IEnumerable<TEntity> entities);
+        //      /// <summary>
+        ///// Counts all the records in the table.
+        ///// </summary>
+        ///// <returns>The record count</returns>
+        //int Count();
+
+        //      /// <summary>
+        //      ///	Check exists any record in the table.
+        //      /// </summary>
+        //      /// <returns>True if any record exists, Otherwise false</returns>
+        //      bool Exists();
+
+        //      /// <summary>
+        //      /// Queries the database for a single record based on its primary key.
+        //      /// </summary>
+        //      /// <param name="primaryKey">Value of primary key field used for filtering.</param>
+        //      /// <returns>Returns single record based on its Guid value</returns>
+        //      TEntity Get(TPrimaryKey primaryKey);
+
+        //      /// <summary>
+        //      /// Queries the database for a single record based on its primary key and deleted filter.
+        //      /// </summary>
+        //      /// <param name="primaryKey">Value of primary key field used for filtering.</param>
+        //      /// <param name="excludeDeleted">Include deleted record.</param>
+        //      /// <returns>Returns single record based on its Guid value</returns>
+        //      TEntity Get(TPrimaryKey primaryKey, bool excludeDeleted);
+
+        //      /// <summary>
+        //      /// Queries the database for a single record based on its primary keys and deleted filter.
+        //      /// </summary>
+        //      /// <param name="entity">The entity from which the primary keys will be extracted and used for filtering.</param>
+        //      /// <returns>Returns single record based on its primary keys</returns>
+             TEntity Get(TEntity entity);
+    }
+
+}
